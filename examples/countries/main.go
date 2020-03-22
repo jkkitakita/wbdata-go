31e29f0ce18f6823d18be5b3be3c25ecf4ae7c00
jkkitakita <jkkitakita@gmail.com>
2020-03-22 15:36:18 +0900

    add: GetCountry

--- examples/countries/main.go
+++ examples/countries/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	client := wbdata.NewClient(nil)
+
+	// ListCountries
 	summary, countries, err := client.Countries.ListCountries(wbdata.PageParams{
 		Page:    1,
 		PerPage: 1,
@@ -23,4 +25,14 @@ func main() {
 	// countries: [{ID:ABW Name:Aruba CapitalCity:Oranjestad Iso2Code:AW Longitude:-70.0167 Latitude:12.5167 Region:{ID:LCN Iso2Code:ZJ Code:} IncomeLevels:{ID: Iso2Code: Value:} LendingType:{ID:LNX Iso2Code:XX Value:Not classified} AdminRegion:{ID: Iso2Code: Value:}}]
 	fmt.Printf("summary: %+v\n", summary)
 	fmt.Printf("countries: %+v\n", countries)
+
+	// GetCountry
+	_, country, err := client.Countries.GetCountry("jpn")
+	if err != nil {
+		fmt.Printf("err: %+v\n", err)
+	}
+
+	// e.g. Outputs:
+	// country: {ID:JPN Name:Japan CapitalCity:Tokyo Iso2Code:JP Longitude:139.77 Latitude:35.67 Region:{ID:EAS Iso2Code:Z4 Code:} IncomeLevels:{ID: Iso2Code: Value:} LendingType:{ID:LNX Iso2Code:XX Value:Not classified} AdminRegion:{ID: Iso2Code: Value:}}
+	fmt.Printf("country: %+v\n", country)
 }
