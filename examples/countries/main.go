16453abf76b7ec84271e8bffd7e984db9e9c19ce
jkkitakita <jkkitakita@gmail.com>
2020-03-22 15:23:51 +0900

    fix: comment for example

--- examples/countries/main.go
+++ examples/countries/main.go
@@ -13,12 +13,14 @@ func main() {
 		PerPage: 1,
 	})
 	if err != nil {
+		// e.g. Outputs:
+		// err: [{ID:150 Key:Language with ISO2 code: 'v3' is not yet supported in the API Value:Response requested in an unsupported language.}]
 		fmt.Printf("err: %+v\n", err)
 	}
 
-	fmt.Printf("summary: %+v\n", summary)
-	fmt.Printf("countries: %+v\n", countries)
 	// e.g. Outputs:
 	// summary: {Page:1 Pages:304 PerPage:1 Total:304}
 	// countries: [{ID:ABW Name:Aruba CapitalCity:Oranjestad Iso2Code:AW Longitude:-70.0167 Latitude:12.5167 Region:{ID:LCN Iso2Code:ZJ Code:} IncomeLevels:{ID: Iso2Code: Value:} LendingType:{ID:LNX Iso2Code:XX Value:Not classified} AdminRegion:{ID: Iso2Code: Value:}}]
+	fmt.Printf("summary: %+v\n", summary)
+	fmt.Printf("countries: %+v\n", countries)
 }
